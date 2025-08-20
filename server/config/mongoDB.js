773777d7b9a8a149d9f6a98910cb1d93e145fa6b
AnishGane <anishgane10@gmail.com>
2025-08-20 22:19:23 +0545

    Enhance MongoDB connection settings to include SSL and TLS options for improved security and compatibility.

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -4,7 +4,12 @@ const mongoDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("MongoDB Connected");
   });
-  await mongoose.connect(`${process.env.MONGO_URI}`);
+  await mongoose.connect(`${process.env.MONGO_URI},{
+    useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true, // make sure SSL is enabled
+    tlsAllowInvalidCertificates: true, // temporary fix if cert mismatch
+    }`);
 };
 
 export default mongoDB;

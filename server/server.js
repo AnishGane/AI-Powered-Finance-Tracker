e846f385f255bb1aaad8f77429ff6175eadc432e
AnishGane <anishgane10@gmail.com>
2025-08-20 23:22:52 +0545

    Reorder middleware initialization in server.js to ensure express.json() is applied before CORS settings.

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ const corsOptions = {
 mongoDB();
 
 //middleware
-app.use(express.json());
 app.use(cors(corsOptions));
+app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/transaction", transactionRoutes);

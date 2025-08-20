6d4de08f2f3af4dc28087c0176cf02b68f4cd8f1
AnishGane <anishgane10@gmail.com>
2025-08-21 00:18:36 +0545

    Remove the option to allow invalid TLS certificates in MongoDB connection settings for improved security.

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -7,7 +7,6 @@ const mongoDB = async () => {
   await mongoose.connect(`${process.env.MONGO_URI},{
     useNewUrlParser: true,
   useUnifiedTopology: true,
-    tlsAllowInvalidCertificates: true
     }`);
 };
 

99f66625491c88f9c08dc569c9ae919c63156999
AnishGane <anishgane10@gmail.com>
2025-08-20 22:36:03 +0545

    hello

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -7,8 +7,6 @@ const mongoDB = async () => {
   await mongoose.connect(`${process.env.MONGO_URI},{
     useNewUrlParser: true,
   useUnifiedTopology: true,
-  ssl: true, // make sure SSL is enabled
-    tlsAllowInvalidCertificates: true, // temporary fix if cert mismatch
     }`);
 };
 
